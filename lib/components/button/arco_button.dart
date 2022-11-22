494736597d011c435158463fd25df03ba43ac1a0
WangJi <jixwang@thoughtworks.com>
2022-11-22 23:21:39 +0800

    chore:重构

--- lib/components/button/arco_button.dart
+++ lib/components/button/arco_button.dart
@@ -1,17 +1,50 @@
 import 'package:arco_flutter/shape/button_shape.dart';
 import 'package:flutter/material.dart';
 
+getButtonMaterialShapeProperty(ArcoButtonShape? shape) {
+  switch (shape) {
+    case ArcoButtonShape.normal:
+      return MaterialStateProperty.all(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+        ),
+      );
+    case ArcoButtonShape.pill:
+      return MaterialStateProperty.all(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(400),
+        ),
+      );
+    case ArcoButtonShape.rect:
+      return MaterialStateProperty.all(
+        const RoundedRectangleBorder(
+          borderRadius: BorderRadius.zero,
+        ),
+      );
+    default:
+      return MaterialStateProperty.all(
+        RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+        ),
+      );
+  }
+}
+
 class ArcoButton extends StatefulWidget {
   final String? text;
   final Widget? child;
   final VoidCallback? onPressed;
   final ArcoButtonShape? shape;
+  final Color? backgroundColor;
+  final Color? textColor;
   const ArcoButton(
     this.text, {
     Key? key,
     required this.onPressed,
     this.child,
     this.shape,
+    this.backgroundColor,
+    this.textColor,
   })  : assert(child != null || text != null),
         super(key: key);
 
@@ -23,6 +56,8 @@ class _ArcoButtonState extends State<ArcoButton> {
   Widget? child;
   MaterialStateProperty<OutlinedBorder?>? shape;
   void Function()? onPressed;
+  Color? backgroundColor;
+  Color? textColor;
   @override
   void initState() {
     super.initState();
